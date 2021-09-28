aac8c53e7d67a15f7c02edc7e0894f469133ab32
hyeseon-park <lightning350@naver.com>
2021-09-28 16:35:36 +0900

    메인 아이템 추가

--- src/components/organism/main/index/content.jsx
+++ src/components/organism/main/index/content.jsx
@@ -1,14 +1,62 @@
 import React, { memo } from "react";
 import styled from "styled-components";
-import { PINK } from "../../../shared/common/colors";
+import { WHITE } from "../../../shared/common/colors";
+import * as I from "../../../shared/styles/item.style";
 
 const ListContent = styled.div`
   width: 100%;
-  background-color: ${PINK};
+  background-color: ${WHITE};
 `;
 
 const Content = memo(() => {
-  return <ListContent>Main Content</ListContent>;
+  return (
+    <ListContent>
+      <I.ItemWrap>
+        <I.ItemLeft>
+          <I.ItemTitle>타이틀 1</I.ItemTitle>
+          <I.ItemDesc>설명 설명 설명 설명 설명 설명</I.ItemDesc>
+          <I.ItemDate>2021.09.28 - 2021.09.28</I.ItemDate>
+        </I.ItemLeft>
+        <I.ItemRight>
+          <I.ItemLove>
+            <I.Img></I.Img>
+          </I.ItemLove>
+          <I.LovePlus>+</I.LovePlus>
+          <I.LoveMinus>-</I.LoveMinus>
+        </I.ItemRight>
+      </I.ItemWrap>
+
+      <I.ItemWrap>
+        <I.ItemLeft>
+          <I.ItemTitle>타이틀 2</I.ItemTitle>
+          <I.ItemDesc>설명 설명 설명 설명 설명 설명</I.ItemDesc>
+          <I.ItemDate>2021.09.28 - 2021.09.28</I.ItemDate>
+        </I.ItemLeft>
+        <I.ItemRight>
+          <I.ItemLove>
+            <I.Img></I.Img>
+          </I.ItemLove>
+          <I.LovePlus>+</I.LovePlus>
+          <I.LoveMinus>-</I.LoveMinus>
+        </I.ItemRight>
+      </I.ItemWrap>
+
+      <I.ItemWrap>
+        <I.ItemLeft>
+          <I.ItemTitle>타이틀 3</I.ItemTitle>
+          <I.ItemDesc>설명 설명 설명 설명 설명 설명</I.ItemDesc>
+          <I.ItemDate>2021.09.28 - 2021.09.28</I.ItemDate>
+        </I.ItemLeft>
+        <I.ItemRight>
+          <I.ItemLove>
+            <I.Img></I.Img>
+          </I.ItemLove>
+          <I.LovePlus>+</I.LovePlus>
+          <I.LoveMinus>-</I.LoveMinus>
+        </I.ItemRight>
+      </I.ItemWrap>
+    </ListContent>
+  );
 });
 
 export default Content;
--- src/components/organism/main/index/header.jsx
+++ src/components/organism/main/index/header.jsx
@@ -1,14 +1,25 @@
 import React, { memo } from "react";
 import styled from "styled-components";
-import { RED } from "../../../shared/common/colors";
+import { BLACK, WHITE } from "../../../shared/common/colors";
+import { MEDIUM } from "../../../shared/common/fontSize";
 
 const Top = styled.div`
   width: 100%;
-  background-color: ${RED};
+  background-color: ${WHITE};
+`;
+
+const Title = styled.p`
+  font-size: ${MEDIUM};
+  color: ${BLACK};
+  line-height: 24px;
 `;
 
 const Header = memo(() => {
-  return <Top>Main Header</Top>;
+  return (
+    <Top>
+      <Title>LOVE IT</Title>
+    </Top>
+  );
 });
 
 export default Header;
