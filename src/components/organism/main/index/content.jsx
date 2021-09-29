5f56b76f07d611b09cadde66a356d9838981c52a
hyeseon-park <lightning350@naver.com>
2021-09-29 10:57:13 +0900

    디테일 페이지 이동 추가

--- src/components/organism/main/index/content.jsx
+++ src/components/organism/main/index/content.jsx
@@ -1,4 +1,5 @@
 import React, { memo } from "react";
+import { useHistory } from "react-router";
 import styled from "styled-components";
 import { WHITE } from "../../../shared/common/colors";
 import * as I from "../../../shared/styles/item.style";
@@ -9,8 +10,16 @@ const ListContent = styled.div`
 `;
 
 const Content = memo(() => {
+  const history = useHistory();
+  const goToDetail = (seq) => {
+    history.push({
+      pathname: "/detail",
+      state: { seq },
+    });
+  };
+
   return (
-    <ListContent>
+    <ListContent onClick={goToDetail(1)}>
       <I.ItemWrap>
         <I.ItemLeft>
           <I.ItemTitle>타이틀 1</I.ItemTitle>
@@ -26,7 +35,7 @@ const Content = memo(() => {
         </I.ItemRight>
       </I.ItemWrap>
 
-      <I.ItemWrap>
+      <I.ItemWrap onClick={goToDetail(2)}>
         <I.ItemLeft>
           <I.ItemTitle>타이틀 2</I.ItemTitle>
           <I.ItemDesc>설명 설명 설명 설명 설명 설명</I.ItemDesc>
@@ -41,7 +50,7 @@ const Content = memo(() => {
         </I.ItemRight>
       </I.ItemWrap>
 
-      <I.ItemWrap>
+      <I.ItemWrap onClick={goToDetail(3)}>
         <I.ItemLeft>
           <I.ItemTitle>타이틀 3</I.ItemTitle>
           <I.ItemDesc>설명 설명 설명 설명 설명 설명</I.ItemDesc>
