83368e7ec50246ad36a443f95138b8867d27f1c3
hyeseon-park <lightning350@naver.com>
2021-09-29 11:44:29 +0900

    임시 데이터 추가

--- src/page/main/index.jsx
+++ src/page/main/index.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import IndexTemplate from "../../templates/main";
 
 const Index = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [items, setItems] = useState([
+    {
+      id: "1",
+      title: "사과 먹기",
+      startDate: Date(),
+      endDate: Date(),
+      describe: "하루에 한 번 사과를 먹는다.",
+    },
+    {
+      id: "2",
+      title: "달리기",
+      startDate: Date(),
+      endDate: Date(),
+      describe: "이틀에 한 번 운동장을 달린다.",
+    },
+    {
+      id: "3",
+      title: "개발 공부하기",
+      startDate: Date(),
+      endDate: Date(),
+      describe: "틈틈이 개발 공부를 한다.",
+    },
+  ]);
+
   return (
     <>
-      <IndexTemplate />
+      <IndexTemplate items={items} />
     </>
   );
 };
