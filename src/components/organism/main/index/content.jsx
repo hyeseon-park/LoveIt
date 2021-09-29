c48ace3ca5a4302f0222f74a1b543f44899cbf57
hyeseon-park <lightning350@naver.com>
2021-09-29 11:39:22 +0900

    메인 디자인 변경

--- src/components/organism/main/index/content.jsx
+++ src/components/organism/main/index/content.jsx
@@ -19,49 +19,55 @@ const Content = memo(() => {
   };
 
   return (
-    <ListContent onClick={goToDetail(1)}>
-      <I.ItemWrap>
+    <ListContent>
+      <I.ItemWrap onClick={goToDetail(1)}>
         <I.ItemLeft>
-          <I.ItemTitle>타이틀 1</I.ItemTitle>
-          <I.ItemDesc>설명 설명 설명 설명 설명 설명</I.ItemDesc>
+          <I.ItemTitle>타이틀 타이틀</I.ItemTitle>
           <I.ItemDate>2021.09.28 - 2021.09.28</I.ItemDate>
+          <I.ItemDesc>설명 설명 설명 설명 설명 설명</I.ItemDesc>
         </I.ItemLeft>
         <I.ItemRight>
           <I.ItemLove>
             <I.Img></I.Img>
           </I.ItemLove>
-          <I.LovePlus>+</I.LovePlus>
-          <I.LoveMinus>-</I.LoveMinus>
+          <I.LoveControl>
+            <I.LovePlus>+</I.LovePlus>
+            <I.LoveMinus>-</I.LoveMinus>
+          </I.LoveControl>
         </I.ItemRight>
       </I.ItemWrap>
 
-      <I.ItemWrap onClick={goToDetail(2)}>
+      <I.ItemWrap onClick={goToDetail(1)}>
         <I.ItemLeft>
-          <I.ItemTitle>타이틀 2</I.ItemTitle>
-          <I.ItemDesc>설명 설명 설명 설명 설명 설명</I.ItemDesc>
+          <I.ItemTitle>타이틀 타이틀</I.ItemTitle>
           <I.ItemDate>2021.09.28 - 2021.09.28</I.ItemDate>
+          <I.ItemDesc>설명 설명 설명 설명 설명 설명</I.ItemDesc>
         </I.ItemLeft>
         <I.ItemRight>
           <I.ItemLove>
             <I.Img></I.Img>
           </I.ItemLove>
-          <I.LovePlus>+</I.LovePlus>
-          <I.LoveMinus>-</I.LoveMinus>
+          <I.LoveControl>
+            <I.LovePlus>+</I.LovePlus>
+            <I.LoveMinus>-</I.LoveMinus>
+          </I.LoveControl>
         </I.ItemRight>
       </I.ItemWrap>
 
-      <I.ItemWrap onClick={goToDetail(3)}>
+      <I.ItemWrap onClick={goToDetail(1)}>
         <I.ItemLeft>
-          <I.ItemTitle>타이틀 3</I.ItemTitle>
-          <I.ItemDesc>설명 설명 설명 설명 설명 설명</I.ItemDesc>
+          <I.ItemTitle>타이틀 타이틀</I.ItemTitle>
           <I.ItemDate>2021.09.28 - 2021.09.28</I.ItemDate>
+          <I.ItemDesc>설명 설명 설명 설명 설명 설명</I.ItemDesc>
         </I.ItemLeft>
         <I.ItemRight>
           <I.ItemLove>
             <I.Img></I.Img>
           </I.ItemLove>
-          <I.LovePlus>+</I.LovePlus>
-          <I.LoveMinus>-</I.LoveMinus>
+          <I.LoveControl>
+            <I.LovePlus>+</I.LovePlus>
+            <I.LoveMinus>-</I.LoveMinus>
+          </I.LoveControl>
         </I.ItemRight>
       </I.ItemWrap>
     </ListContent>
