19d7e2653cb822f4a2dbc079a16267d6a269a481
Hyeseon <hsvogo@gmail.com>
2022-01-22 14:45:01 +0900

    헤더 공통화

--- src/components/organism/detail/index/header.jsx
+++ src/components/organism/detail/index/header.jsx
@@ -1,14 +1,8 @@
 import React, { memo } from "react";
-import styled from "styled-components";
-import { RED } from "../../../shared/common/colors";
-
-const Top = styled.div`
-  width: 100%;
-  background-color: ${RED};
-`;
+import TitleHeader from "../../../header/header";
 
 const Header = memo(() => {
-  return <Top>Detail Header</Top>;
+  return <TitleHeader title={"detail header"} />;
 });
 
 export default Header;
